15d85e3855f52b75bed85b1ec032e11c7354559c
BerellExpress <yury1997@mail.ru>
2024-11-19 22:04:50 +0300

    Add files via upload

---
+++ RunnerTest.py
@@ -0,0 +1,26 @@
+import main
+import unittest
+
+class RunnerTest(unittest.TestCase):
+    def test_walk(self):
+        runner = main.Runner("Walker")
+        for i in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = main.Runner("Rooney")
+        for i in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = main.Runner("Alice")
+        runner2 = main.Runner("Totoshka")
+        for i in range(10):
+            runner1.run()
+            runner2.run()
+        self.assertEqual(runner1.distance, runner2.distance)
+
+if __name__ == "__main__":
+    main
